89492d82e39d5fa44c10b98fb0dde8b91d3c01b1
L8ton-crypto <L8ton-crypto@users.noreply.github.com>
2026-02-08 17:26:40 +0000

    fix: flip spinner triangle to point at wheel

--- src/components/SpinWheel.tsx
+++ src/components/SpinWheel.tsx
@@ -38,9 +38,9 @@ export default function SpinWheel({ isSpinning, onSpin, meal }: SpinWheelProps) 
     <div className="flex flex-col items-center">
       {/* Wheel Container */}
       <div className="relative mb-8">
-        {/* Pointer */}
-        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-2 z-20">
-          <div className="w-0 h-0 border-l-[12px] border-r-[12px] border-b-[20px] border-l-transparent border-r-transparent border-b-yellow-400 drop-shadow-lg"></div>
+        {/* Pointer - points DOWN at the wheel */}
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1 z-20">
+          <div className="w-0 h-0 border-l-[12px] border-r-[12px] border-t-[20px] border-l-transparent border-r-transparent border-t-yellow-400 drop-shadow-lg"></div>
         </div>
 
         {/* Wheel */}
